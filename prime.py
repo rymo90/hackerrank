d9403a18224777e8bab565fcd9ccd3ac83c19d01
rymo90 <red.01yassin@gmail.com>
2018-08-18 00:04:35 +0200

    prime update

--- prime.py
+++ prime.py
@@ -1,9 +1,22 @@
-def is_prime(a):
-    return all(a % i for i in range(2, a))
+import math
+
+
+def is_prime(n):
+    if n == 2:
+        return True
+    if n % 2 == 0 or n <= 1:
+        return False
+
+    sqr = int(math.sqrt(n)) + 1
+
+    for divisor in range(3, sqr, 2):
+        if n % divisor == 0:
+            return False
+    return True
 
 
 n = int(input())
-for _ in range(3):
+for _ in range(n):
     x = int(input())
     svar = is_prime(x)
     if svar:
